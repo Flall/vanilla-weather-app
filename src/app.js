13e2b4d01b98ce45fb55559e2db73c2d1ddd09d8
Flall <119894671+Flall@users.noreply.github.com>
2023-01-17 22:28:31 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#current-temperature");
@@ -10,6 +34,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let currentDateElement = document.querySelector("#current-date");
+  currentDateElement.innerHTML = formatDate(response.data.time);
 }
 
 let apiKey = "1c0f6e49a911db65307b85186bd4t6oe";
