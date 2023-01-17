5443297c3049461f57d6db158cac3ee2e5b84a8c
Flall <119894671+Flall@users.noreply.github.com>
2023-01-17 21:57:11 +0100

    Added JS & Axios & API
    Building JS structure to retrieve live-data info on weather by using API and Axios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "1c0f6e49a911db65307b85186bd4t6oe";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Amsterdam&key=${apiKey}`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
